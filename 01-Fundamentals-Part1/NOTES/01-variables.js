1df777a1d04099545683d625c39db692bca4de34
chaithrareddy47 <chaithrareddy475@gmail.com>
2026-07-15 09:26:48 +0530

    first commit

---
+++ 01-Fundamentals-Part1/NOTES/01-variables.js
@@ -0,0 +1,72 @@
+// ==========================================
+// VARIABLES
+// ==========================================
+
+// A value is a piece of data.
+// Examples:
+// "Jonas" -> String
+// 23 -> Number
+// true -> Boolean
+
+console.log("Jonas");
+
+// Declaring a variable
+let firstName = "Namaste";
+
+console.log(firstName);
+
+// console.log() is used to print output to the console.
+
+/*
+===========================
+Variable Naming Rules
+===========================
+
+1. Use camelCase
+   Example:
+   firstName
+   studentAge
+   totalMarks
+
+2. Variable names cannot start with a number.
+   ❌ let 1name = "Akshay";
+   ✅ let name1 = "Akshay";
+
+3. Variable names can contain:
+   - Letters (a-z, A-Z)
+   - Numbers (not at the beginning)
+   - Underscore (_)
+   - Dollar sign ($)
+
+4. Variable names cannot contain spaces.
+   ❌ first name
+   ✅ firstName
+
+5. Do not use JavaScript reserved keywords.
+   Example:
+   let, const, function, if, return
+
+6. Variable names are case-sensitive.
+   age !== Age
+
+7. Variable names should be meaningful.
+   ❌ let x = 22;
+   ✅ let studentAge = 22;
+
+8. By convention:
+   Variables -> camelCase
+   Constants -> UPPER_CASE (optional)
+   Classes -> PascalCase
+*/
+
+/*
+Best Practices
+
+✔ Use meaningful variable names.
+
+✔ Use const by default.
+
+✔ Use let only when the value changes.
+
+✔ Avoid using var in modern JavaScript.
+*/
---
+++ 01-Fundamentals-Part1/PRACTICE/01-variables-practice.js
@@ -0,0 +1,84 @@
+const firstName = "chaithra";
+const age = 28;
+const city = "Bangalore";
+
+console.log(firstName);
+console.log(age);
+console.log(city);
+
+// Problem 2 - Store your favorite movie.
+const movie = "The brightest start in teh sky";
+console.log(movie);
+
+// Problem 3 - Store marks.
+const math = 23;
+const science = 45;
+const english = 99;
+
+console.log(math);
+console.log(science);
+console.log(english);
+
+// Problem 4 Store your dream company.
+const company = "microsoft";
+console.log(company);
+
+// Problem 5 - Store Boolean values.
+const isStudent = true;
+const hasJob = false;
+console.log(isStudent);
+console.log(hasJob);
+
+// 🟡 Medium Problems
+// Problem 6 Swap two variables.
+let a = 10;
+let b = 20;
+// swap numbeer
+let c = a;
+a = b;
+b = a;
+a = c;
+
+console.log(b);
+console.log(c);
+
+// Problem 7 - Create variables and print a sentence.
+
+const name = "Akshay";
+const job = "teaches namaste js";
+
+console.log(name + " " + job);
+
+//Problem 8 - Store prices.
+const applePrice = 30;
+const bananaPrice = 20;
+const mangoPrice = 50;
+
+console.log(applePrice);
+
+// Problem 9 - Create constants.
+const PI = 3.14159;
+const COUNTRY = "India";
+
+console.log(PI);
+
+//  small challenge
+const name1 = "chaithra";
+const Age = "28";
+const College = "Sadhguru sainath";
+const City = "Bangalore";
+const Country = "India";
+const dreamCompany = "Microsoft";
+const expectedSalary = "5LPA";
+const favoriteProgrammingLanguage = "javascript";
+
+console.log("============ My Profile ==============");
+console.log("Name : " + " " + name1);
+console.log("College : " + " " + College);
+console.log("City : " + " " + City);
+console.log("DreamCompany : " + " " + dreamCompany);
+console.log("ExpectedSalary : " + " " + expectedSalary);
+console.log(
+  "favoriteProgrammingLanguage : " + " " + favoriteProgrammingLanguage,
+);
+console.log("=========================================");
